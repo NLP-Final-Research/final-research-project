1763ea075ebdbef20dfe51442a628511ee42d24e
yeonieheoo <sh5874@nyu.edu>
2022-11-19 23:12:51 -0500

    Edit Data Collection

--- data-collection.py
+++ data-collection.py
@@ -12,29 +12,23 @@ def printtweetdata(n, ith_tweet):
  
 # function to perform data extraction
 def scrape(words, date_since, numtweet):
-    # Creating DataFrame using pandas
     db = pd.DataFrame(columns=['username', 'text', 'hashtags'])
  
-    # We are using .Cursor() to search
-    # through twitter for the required tweets.
+    # We are using .Cursor() to search through twitter for the required tweets.
     # The number of tweets can be restricted using .items(number of tweets)
     tweets = tweepy.Cursor(api.search_tweets,
                             words, lang="en",
                            since_id=date_since,
                            tweet_mode='extended').items(numtweet)
  
- 
-    # .Cursor() returns an iterable object. Each item in
-    # the iterator has various attributes
-    # that you can access to
-    # get information about each tweet
+    # .Cursor() returns an iterable object. Each item in the iterator has various attributes
+    # that you can access to get information about each tweet
     list_tweets = [tweet for tweet in tweets]
  
     # Counter to maintain Tweet Count
     i = 1
  
-    # we will iterate over each tweet in the
-    # list for extracting information about each tweet
+    # we will iterate over each tweet in the list for extracting information about each tweet
     for tweet in list_tweets:
         username = tweet.user.screen_name
         hashtags = tweet.entities['hashtags']
@@ -56,14 +50,11 @@ def scrape(words, date_since, numtweet):
         i = i+1
     
     filename = 'scraped_tweets.csv'
- 
-    # we will save our database as a CSV file.
     db.to_csv(filename)
 
 if __name__ == '__main__':
  
-    # Enter your own credentials obtained
-    # from your developer account
+    # Enter your own credentials obtained from your developer account
     consumer_key = "XXXXXXXXXXXXXXXXXXXXX"
     consumer_secret = "XXXXXXXXXXXXXXXXXXXXX"
     access_key = "XXXXXXXXXXXXXXXXXXXXX"
